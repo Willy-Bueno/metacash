02014a6cd4e845551bc83c82ba944c60494a5bf1
Willy-Bueno <willybueno090@gmail.com>
2023-01-09 11:11:27 -0300

    update code

--- src/helper/axios.ts
+++ src/helper/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `http://localhost:3000/api`,
+  baseURL: `https://metacash.vercel.app/api`,
   headers: {
     'Content-Type': 'multipart/form-data',
   }
